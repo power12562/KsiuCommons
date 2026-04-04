051a9260bf80534f0bc4b05c12c369206a4b7a60
power12562 <kimsiwoo1005@gmail.com>
2026-04-04 15:41:31 +0900

    chore(project): 프로젝트 설정 변경

--- StreamConnector/build.gradle.kts
+++ StreamConnector/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 group =
@@ -23,8 +24,23 @@ dependencies {
     testRuntimeOnly(
         "org.junit.platform:junit-platform-launcher"
     )
+
+    implementation("io.socket:socket.io-client:1.0.2")
+    implementation("org.json:json:20231013")
 }
 
-tasks.test {
-    useJUnitPlatform()
+val mainPackage = "com.ksiu.commons"
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+    shadowJar {
+        relocate("io.socket", "$mainPackage.shadow.io.socket")
+        relocate("org.json", "$mainPackage.shadow.org.json")
+        relocate("okhttp3", "$mainPackage.shadow.okhttp3")
+        relocate("okio", "$mainPackage.shadow.okio")
+    }
+    jar {
+        enabled = false
+    }
 }
