cc100d91fa3870cc5fab497ce0bf67656ddf5706
power12562 <kimsiwoo1005@gmail.com>
2026-04-04 21:54:07 +0900

    feature(core): revoke 함수 하나로 통일 및 구독 이벤트 해지하도록 개선

--- StreamConnector/src/main/java/com/ksiu/commons/streamconnector/token/ChzzkToken.java
+++ StreamConnector/src/main/java/com/ksiu/commons/streamconnector/token/ChzzkToken.java
@@ -1,21 +1,26 @@
 package com.ksiu.commons.streamconnector.token;
 
 import com.ksiu.commons.streamconnector.authorizer.ChzzkAuthorizer;
+import com.ksiu.commons.streamconnector.session.ChzzkSession;
 
 public final class ChzzkToken
 {
+    private static final long VALID_TOKEN_TIME_MS = 1430 * 60 * 1000L;
     private final String _accessToken;
     private final String _refreshToken;
+    private final String _channelId;
+    private final String _channelName;
     private final ChzzkAuthorizer _authorizer;
     private boolean _isValid;
-    private static final long VALID_TOKEN_TIME_MS = 1430 * 60 * 1000L;
     private final long _issuedAt;
 
-    public ChzzkToken(ChzzkAuthorizer authorizer, String accessToken, String refreshToken)
+    public ChzzkToken(ChzzkAuthorizer authorizer, String accessToken, String refreshToken, String channelId, String channelName)
     {
         _authorizer = authorizer;
         _accessToken = accessToken;
         _refreshToken = refreshToken;
+        _channelId = channelId;
+        _channelName = channelName;
         _issuedAt = System.currentTimeMillis();
         _isValid = true; // 하루 뒤 false 되야함 revoke 에서도 false로 해야함
     }
@@ -30,18 +35,39 @@ public final class ChzzkToken
         return _refreshToken;
     }
 
+    public final String getChannelId()
+    {
+        return _channelId;
+    }
+
+    public final String getChannelName()
+    {
+        return _channelName;
+    }
+
     public final boolean IsValid()
     {
         return _isValid && (System.currentTimeMillis() < _issuedAt + VALID_TOKEN_TIME_MS);
     }
 
-    public final void revokeAccessToken()
+    public void revoke()
+    {
+        revokeAccessToken();
+        revokeRefreshToken();
+        ChzzkSession mySession = ChzzkSession.getSessionByToken(this);
+        if (mySession != null)
+        {
+            mySession.unsubscribeTokenEvents(this);
+        }
+    }
+
+    private final void revokeAccessToken()
     {
         _isValid = false;
         _authorizer.revokeToken(getAccessToken(), "access_token");
     }
 
-    public final void revokeRefreshToken()
+    private final void revokeRefreshToken()
     {
         _isValid = false;
         _authorizer.revokeToken(getRefreshToken(), "refresh_token");
